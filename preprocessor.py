253fc8993573443795e0d720749d011e456e1aa8
Cravat5386 <56939620+Cravat5386@users.noreply.github.com>
2021-05-31 21:55:06 -0400

    Started Preprocessor
    Created Preprocessor file
    Wrote Hilbert Curve Function (Untested)
     - draw_hilbert creates a hilbert curve given a dimension of 2^n
     - while this doesn't cover all possible cases, we can choose the number of mel tones accordingly to compensate

---
+++ preprocessor.py
@@ -0,0 +1,27 @@
+def draw_hilbert(dim, startx = 0, starty = 0, pos_step = True, xfirst = False):
+    if dim == 2:
+        if pos_step:
+            if not xfirst:
+                return [(startx, starty), (startx, starty+1), (startx+1, starty+1), (startx+1, starty)]
+            else:
+                return [(startx, starty), (startx+1, starty), (startx+1, starty+1), (startx, starty+1)]
+        else:
+            if not xfirst:
+                return [(startx, starty), (startx, starty-1), (startx-1, starty-1), (startx-1, starty)]
+            else:
+                return [(startx, starty), (startx-1, starty), (startx-1, starty-1), (startx, starty-1)]
+    else:
+        sign = 1
+        if not pos_step:
+            sign = -1
+        if not xfirst:
+            startx2, starty2 = startx, starty+dim*sign/2
+            startx3, starty3 = startx2+dim*sign/2, starty2
+            startx4, starty4 = startx3+dim*sign/4, starty3-dim*sign/4
+        else:
+            startx2, starty2 = startx+dim*sign/2, starty
+            startx3, starty3 = startx2, starty2+dim*sign/2
+            startx4, starty4 = startx3-dim*sign/4, starty3+dim*sign/4
+        return draw_hilbert(dim/2, startx, starty, pos_step, not xfirst) + draw_hilbert(dim/2, startx2, starty2, pos_step, xfirst) + draw_hilbert(dim/2, startx3, starty3, pos_step, xfirst) + draw_hilbert(dim/2, startx4, starty4, not pos_step, not xfirst)
+
+        
