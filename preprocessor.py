2da6c834c1c62ad0661bf0091622d9f44647b9ed
Cravat5386 <56939620+Cravat5386@users.noreply.github.com>
2021-06-01 10:28:00 -0400

    Bug fixes
    Verified that it works now

--- preprocessor.py
+++ preprocessor.py
@@ -17,11 +17,9 @@ def draw_hilbert(dim, startx = 0, starty = 0, pos_step = True, xfirst = False):
         if not xfirst:
             startx2, starty2 = startx, starty+dim*sign/2
             startx3, starty3 = startx2+dim*sign/2, starty2
-            startx4, starty4 = startx3+dim*sign/4, starty3-dim*sign/4
+            startx4, starty4 = startx3+(dim/2-1)*sign, starty3-sign
         else:
             startx2, starty2 = startx+dim*sign/2, starty
             startx3, starty3 = startx2, starty2+dim*sign/2
-            startx4, starty4 = startx3-dim*sign/4, starty3+dim*sign/4
+            startx4, starty4 = startx3-sign, starty3+(dim/2-1)*sign
         return draw_hilbert(dim/2, startx, starty, pos_step, not xfirst) + draw_hilbert(dim/2, startx2, starty2, pos_step, xfirst) + draw_hilbert(dim/2, startx3, starty3, pos_step, xfirst) + draw_hilbert(dim/2, startx4, starty4, not pos_step, not xfirst)
-
-        
