451630f904d930d5ad40a78f247f4236bf1294b7
qed11 <ericdai1130@gmail.com>
2021-06-05 21:12:54 -0400

    small amendment

--- CNN.py
+++ CNN.py
@@ -7,7 +7,7 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 5, 3, padding = 1) #224*224*3 - > 224*224*5
-        self.pool = nn.MaxPool2d(2, 2) #112*112*5 - 56*56*10 - 28*28*20
+        self.pool = nn.MaxPool2d(2, 2) #112*112*5 - 56*56*10 - 28*28*20 - 14*14*40
         self.conv2 = nn.Conv2d(5, 10, 3, padding = 1) #112*112*10 -> 112*112*10
         self.conv3 = nn.Conv2d(10,20,3, padding = 1) #56*56*10 -> 56*56*20
         self.conv4 = nn.Conv2d(20,40,3, padding = 1) #28*28*20 -> 28*28*40
